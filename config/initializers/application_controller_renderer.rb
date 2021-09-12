7e4d784c4a1e5661cc04a8bd37fe525f218c75e9
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-09-12 05:39:53 +0000

    [show]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '20f2b65f6ce969b6fd3bbb5782489ad3f296299fb66ecb3cc093e5ddb13254d64e22b22f315d25825f558cb5ed35c85ee6d394447b13e21681ca7a6ab36b43f2'
